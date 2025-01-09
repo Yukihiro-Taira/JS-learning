a868c6343285b83bce526fde04cc9a1bad672a7b
Yukihiro Taira <lowfreqnoise@gmail.com>
2025-01-08 21:48:52 -0800

    no message

--- mainProject/main/js/textPop.js
+++ mainProject/main/js/textPop.js
@@ -1,5 +1,7 @@
 let canvas;
 let ctx;
+let flowField;
+let flowFieldAnimation
 
 window.addEventListener('load', function(){
     canvas = document.getElementById('canvas1');
@@ -9,6 +11,94 @@ window.addEventListener('load', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     console.log(ctx);
+
+
+    class FlowFieldEffect{
+        #ctx;
+        #width;
+        #height;
+        constructor(ctx, width, height){
+            this.#ctx = ctx;
+            this.#width = width;
+            this.#height = height*2;
+            this.#ctx.lineWidth = 2.5;
+            this.#ctx.strokeStyle = 'white';
+            this.lastTime = 0;
+            this.interval = 1000/60;
+            this.timer = 0;
+            this.cellSize = 15;
+            this.gradient;
+            this.#createGradient();
+            this.#ctx.strokeStyle = this.gradient;
+            this.radius = 1;
+            this.vel =0.001;
+
+            this.mouse ={
+                radius:20000,
+                x: 0,
+                y: 0
+            }
+            window.addEventListener('mousemove', (e) =>{
+                this.mouse.x = e.x;
+                this.mouse.y = e.y;
+            })
+
+
+        }
+    
+        #createGradient(){
+    
+            this.gradient = this.#ctx.createLinearGradient(0,0,this.#width,this.#height);
+            this.gradient.addColorStop('0.1','#ff0133');
+            this.gradient.addColorStop('0.3','blue');
+            this.gradient.addColorStop('0.5','teal');
+            this.gradient.addColorStop('0.7','#555c33');
+            this.gradient.addColorStop('0.9','#ffff33');
+    
+        }
+    
+        #drawLine(angle,x, y){
+
+            let posx = x;
+            let posy = y;
+            
+            let dx = this.mouse.x - posx;
+            let dy = this.mouse.y - posy;
+            let dist = dx*dx+dy*dy;
+            let length = dist * 0.0001;
+            
+            this.#ctx.soruceCompositeOperation = 'destination-out'
+
+            this.#ctx.beginPath();
+            this.#ctx.moveTo(x, y);
+            this.#ctx.lineTo(x+ + Math.cos(angle) * length,y + Math.sin(angle) * length);
+            this.#ctx.stroke();
+        }
+    
+        animate(timeStamp){
+            const deltaTime = timeStamp - this.lastTime;
+            this.lastTime = timeStamp;
+            if (this.timer > this.interval){
+                this.#ctx.clearRect(0, 0, this.#width, this.#height)
+                this.radius += this.vel;
+                if (this.radius > 0.1 || this.radius < -0.1 ) this.vel *= -1; 
+    
+                for (let y = 0; y < this.#width; y+= this.cellSize){
+                    for (let x = 0; x < (this.#height)*2; x += this.cellSize){
+                        const angle = (Math.cos(x*0.01) + Math.sin(y*0.01)) * this.radius;
+                        this.#drawLine(angle,x,y);
+                        //console.log(angle);
+                    }
+                };
+    
+                this.timer = 0;
+            } else {
+                this.timer += deltaTime;
+            }
+            //console.log(deltaTime);
+            flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
+        }
+    }
 
     class Particle{
         constructor(effect, x, y, color){
@@ -30,8 +120,12 @@ window.addEventListener('load', function(){
             this.ease = Math.random() *0.1 +0.005;
 
 
+            
+
+
         }
         draw(){
+            ctx.globalCompositeOperaton = "source-over"
             this.effect.context.fillStyle = this.color;
             this.effect.context.fillRect(this.x, this.y, this.size, this.size);
         }
@@ -148,6 +242,7 @@ window.addEventListener('load', function(){
             console.log(this.particles);
         }
         render(){
+        ctx.globalCompositeOperaton = "source-over"
             this.particles.forEach(particle => {
                 particle.update();
                 particle.draw();
@@ -175,18 +270,24 @@ window.addEventListener('load', function(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         effect.render();
         requestAnimationFrame(animate);
-
-
-
     }
     animate();
 
 
+
+
+
+
     window.addEventListener('resize', function(){
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+    cancelAnimationFrame(flowFieldAnimation);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log(`${canvas.width} x ${canvas.height}`)
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate(0);
         effect.resize(canvas.width, canvas.height);
         effect.wrapText(effect.textInput.value);
     });
-
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate(0);
 });
---
+++ mainProject/main/js/vectorField.js
@@ -0,0 +1,105 @@
+// let canvas;
+// let ctx;
+let flowField;
+let flowFieldAnimation
+
+window.onload = function(){
+    canvas2 = document.getElementById('canvas2');
+    ctx2 = canvas.getContext('2d');
+    canvas2.width = window.innerWidth;
+    canvas2.height = window.innerHeight;
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate(0);
+}
+const mouse = {
+    x: 0,
+    y: 0,
+}
+
+
+window.addEventListener('mousemove', function(e){
+    mouse.x = e.x;
+    mouse.y = e.y;
+    flowField.animate(0);
+});
+
+window.addEventListener('resize', function(){
+    cancelAnimationFrame(flowFieldAnimation);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log(`${canvas.width} x ${canvas.height}`)
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate(0);
+});
+
+class FlowFieldEffect{
+    #ctx;
+    #width;
+    #height;
+    constructor(ctx, width, height){
+        this.#ctx = ctx;
+        this.#width = width;
+        this.#height = height*2;
+        this.#ctx.lineWidth = 2.5;
+        this.#ctx.strokeStyle = 'white';
+        this.lastTime = 0;
+        this.interval = 1000/60;
+        this.timer = 0;
+        this.cellSize = 15;
+        this.gradient;
+        this.#createGradient();
+        this.#ctx.strokeStyle = this.gradient;
+        this.radius = 1;
+        this.vel =0.001;
+    }
+
+    #createGradient(){
+
+        this.gradient = this.#ctx.createLinearGradient(0,0,this.#width,this.#height);
+        this.gradient.addColorStop('0.1','#ff0133');
+        this.gradient.addColorStop('0.3','blue');
+        this.gradient.addColorStop('0.5','teal');
+        this.gradient.addColorStop('0.7','#555c33');
+        this.gradient.addColorStop('0.9','#ffff33');
+
+    }
+
+    #drawLine(angle,x, y){
+        let posx = x;
+        let posy = y;
+        
+        let dx = mouse.x - posx;
+        let dy = mouse.y - posy;
+        let dist = dx*dx+dy*dy;
+        let length = dist * 0.0001;
+
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(x, y);
+        this.#ctx.lineTo(x+ + Math.cos(angle) * length,y + Math.sin(angle) * length);
+        this.#ctx.stroke();
+    }
+
+    animate(timeStamp){
+        const deltaTime = timeStamp - this.lastTime;
+        this.lastTime = timeStamp;
+        if (this.timer > this.interval){
+            this.#ctx.clearRect(0, 0, this.#width, this.#height)
+            this.radius += this.vel;
+            if (this.radius > 0.1 || this.radius < -0.1 ) this.vel *= -1; 
+
+            for (let y = 0; y < this.#width; y+= this.cellSize){
+                for (let x = 0; x < (this.#height)*2; x += this.cellSize){
+                    const angle = (Math.cos(x*0.01) + Math.sin(y*0.01)) * this.radius;
+                    this.#drawLine(angle,x,y);
+                    //console.log(angle);
+                }
+            };
+
+            this.timer = 0;
+        } else {
+            this.timer += deltaTime;
+        }
+        //console.log(deltaTime);
+        flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
+    }
+}

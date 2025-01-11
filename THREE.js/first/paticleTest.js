8b5038a9506c3ee9d269e0b9d334b7a405c3192c
Yukihiro Taira <lowfreqnoise@gmail.com>
2025-01-10 19:38:35 -0800

    no message

--- THREE.js/first/paticleTest.js
+++ THREE.js/first/paticleTest.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+import { lightPosition } from 'three/src/nodes/TSL.js';
 //Setup//
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -14,9 +15,20 @@ document.body.appendChild( renderer.domElement);
 
 //Variables//
 const vertices = [];
-const POPcount = 10000;
-const POPmax = 2000/2;
-const POPmin = -2000/2;
+const POPcount = 100000;
+const POPmax = 500/2;
+const POPmin = -1000/2;
+const mouse = {
+	x: 0,
+	y: 0,
+}
+
+
+
+document.addEventListener('mousemove', (e)=>{
+	mouse.x = e.x*0.01;
+	mouse.y = e.y*0.01;
+})
 
 for ( let i = 0; i < POPcount; i++ ) {
 	const x = Math.random([i])*(POPmax-POPmin)+POPmin;
@@ -25,24 +37,58 @@ for ( let i = 0; i < POPcount; i++ ) {
 
 	vertices.push( x, y, z );
 }
-
-
-
-const geometry = new THREE.InstancedBufferGeometry();
+//geo
+const sphere = new THREE.SphereGeometry(1,32,16)
+const geometry = new THREE.BufferGeometry();
 geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+//Materials
+const sphereMat = new THREE.MeshStandardMaterial({
+	color: 'grey',
 
+})
+const material = new THREE.PointsMaterial( { 
+	color: 0x888888,
+	size: 1
 
-const material = new THREE.PointsMaterial( { color: 0x888888 } );
+} );
+
+//Mesh Assignment
+const sphereMesh = new THREE.Mesh(sphere,sphereMat);
 const points = new THREE.Points( geometry, material );
 
+//Lights
+const light1 = new THREE.PointLight(0xffffff, 1, 5, 1);
+light1.position.set(0,1.3,0)
+sphereMesh.position.set(0,0,-1)
 
-scene.add( points );
-console.log(vertices);
+//Camera
 camera.position.z = 5;
 
+console.log(light1)
+
+
+scene.add(sphereMesh);
+scene.add(light1);
+scene.add( points );
+
+//Render
+// renderer.setClearColor(new THREE.Color('#21282a'), 1);
 
 function animate(){
 
+
+
+	light1.position.x += .001;
+	light1.position.y += .001;
+
+
+
     renderer.render(scene,camera)
+
+	// console.log(points.position)
+	points.position.x += 0
+	points.position.y += 0
+	points.position.z += 0.01
+	
 }
 renderer.setAnimationLoop (animate);
